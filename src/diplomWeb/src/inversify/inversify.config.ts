369b68fa0f716a8911caa7d1ab91b795ccb9fd5c
Artem Evdokimov <>
2023-04-30 15:58:55 +0300

    added inversify

---
+++ src/diplomWeb/src/inversify/inversify.config.ts
@@ -0,0 +1,15 @@
+import { Container } from 'inversify';
+import 'reflect-metadata';
+
+const container = new Container();
+
+// service
+// service end
+
+// stores
+// stores end
+
+// signalR
+// signalR end
+
+export default container;
