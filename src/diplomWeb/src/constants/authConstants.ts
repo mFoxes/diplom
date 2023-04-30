482eb863f354d1fd946f5cf5afddcb3c0c87ec7b
Artem Evdokimov <>
2023-04-30 16:38:39 +0300

    added auth constants

---
+++ src/diplomWeb/src/constants/authConstants.ts
@@ -0,0 +1,7 @@
+const urlSearchParamsTypeConstants = {
+	grantType: 'grant_type',
+	userName: 'username',
+	password: 'password',
+	clientId: 'client_id',
+	refreshToken: 'refresh_token',
+};
--- src/diplomWeb/src/service/api/authService.ts
+++ src/diplomWeb/src/service/api/authService.ts
@@ -2,6 +2,7 @@ import { AxiosApi } from './base/axiosApi';
 import { Either } from '@sweet-monads/either';
 import { jwtResponse } from '../../models/interfaces/response/jswResponse';
 import { CLIENT_ID } from '../../staticData';
+import { injectable } from 'inversify';
 
 const configToJwt = {
 	headers: {
@@ -9,6 +10,7 @@ const configToJwt = {
 	},
 };
 
+@injectable()
 export default class AuthService extends AxiosApi {
 	public async authRequest(params: URLSearchParams): Promise<Either<unknown, jwtResponse>> {
 		const req = this._post<jwtResponse>({ url: 'connect/token', payload: params, config: configToJwt });
@@ -17,19 +19,19 @@ export default class AuthService extends AxiosApi {
 
 	public async login(username: string, password: string): Promise<Either<unknown, jwtResponse>> {
 		const params = new URLSearchParams();
-		params.append('grant_type', 'password');
-		params.append('username', username);
-		params.append('password', password);
-		params.append('client_id', CLIENT_ID);
+		params.append(urlSearchParamsTypeConstants.grantType, 'password');
+		params.append(urlSearchParamsTypeConstants.userName, username);
+		params.append(urlSearchParamsTypeConstants.password, password);
+		params.append(urlSearchParamsTypeConstants.clientId, CLIENT_ID);
 
 		return this.authRequest(params);
 	}
 
 	public async refresh(refresh_token: string): Promise<Either<unknown, jwtResponse>> {
 		const params = new URLSearchParams();
-		params.append('grant_type', 'refresh_token');
-		params.append('refresh_token', refresh_token);
-		params.append('client_id', CLIENT_ID);
+		params.append(urlSearchParamsTypeConstants.grantType, 'refresh_token');
+		params.append(urlSearchParamsTypeConstants.refreshToken, refresh_token);
+		params.append(urlSearchParamsTypeConstants.clientId, CLIENT_ID);
 
 		return this.authRequest(params);
 	}
