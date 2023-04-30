b5bfe8e0f1235b4c57655488da76c894464397f7
Artem Evdokimov <>
2023-04-30 17:33:17 +0300

    refactor local storage service

---
+++ src/diplomWeb/src/constants/localstorageConstants.ts
@@ -0,0 +1,7 @@
+export const ACCESS_TOKEN = 'access_token';
+export const REFRESH_TOKEN = 'refresh_token';
+export const EXPIRES_IN = 'expires_in';
+export const TIME_TOKEN_CREATE = 'time_token_create';
+export const REMEMBER_ME = 'remember';
+
+export const THEME_MODE = 'theme_mode';
--- src/diplomWeb/src/service/LocalStorageService.ts
+++ src/diplomWeb/src/service/LocalStorageService.ts
@@ -1,28 +1,28 @@
 import { PaletteMode } from '@mui/material';
 import { jwtResponse } from '../models/interfaces/response/jswResponse';
+import {
+	ACCESS_TOKEN,
+	REFRESH_TOKEN,
+	EXPIRES_IN,
+	TIME_TOKEN_CREATE,
+	REMEMBER_ME,
+	THEME_MODE,
+} from '../constants/localstorageConstants';
 
 export default class LocalStorageService {
-	private readonly ACCESS_TOKEN = 'access_token';
-	private readonly REFRESH_TOKEN = 'refresh_token';
-	private readonly EXPIRES_IN = 'expires_in';
-	private readonly TIME_TOKEN_CREATE = 'time_token_create';
-	private readonly REMEMBER_ME = 'remember';
-
-	private readonly THEME_MODE = 'theme_mode';
-
 	public saveJwt(data: jwtResponse): void {
-		localStorage.setItem(this.ACCESS_TOKEN, data.access_token);
-		localStorage.setItem(this.REFRESH_TOKEN, data.refresh_token);
-		localStorage.setItem(this.EXPIRES_IN, data.expires_in.toString());
-		localStorage.setItem(this.TIME_TOKEN_CREATE, (Date.now() / 1000).toString());
+		localStorage.setItem(ACCESS_TOKEN, data.access_token);
+		localStorage.setItem(REFRESH_TOKEN, data.refresh_token);
+		localStorage.setItem(EXPIRES_IN, data.expires_in.toString());
+		localStorage.setItem(TIME_TOKEN_CREATE, (Date.now() / 1000).toString());
 	}
 
 	public saveRememberMe(remember: boolean): void {
-		localStorage.setItem(this.REMEMBER_ME, remember ? 'true' : 'false');
+		localStorage.setItem(REMEMBER_ME, remember ? 'true' : 'false');
 	}
 
 	public getExpiresIn(): number {
-		const expires_in = localStorage.getItem(this.EXPIRES_IN);
+		const expires_in = localStorage.getItem(EXPIRES_IN);
 		if (expires_in !== null) {
 			return parseInt(expires_in);
 		} else {
@@ -31,7 +31,7 @@ export default class LocalStorageService {
 	}
 
 	public getTimeTokenCreateInSeconds(): number {
-		const time_token_create = localStorage.getItem(this.TIME_TOKEN_CREATE);
+		const time_token_create = localStorage.getItem(TIME_TOKEN_CREATE);
 		if (time_token_create !== null) {
 			return parseInt(time_token_create);
 		} else {
@@ -40,17 +40,17 @@ export default class LocalStorageService {
 	}
 
 	public getAccessToken(): string | null {
-		const access_token = localStorage.getItem(this.ACCESS_TOKEN);
+		const access_token = localStorage.getItem(ACCESS_TOKEN);
 		return access_token;
 	}
 
 	public getRefreshToken(): string | null {
-		const refresh_token = localStorage.getItem(this.REFRESH_TOKEN);
+		const refresh_token = localStorage.getItem(REFRESH_TOKEN);
 		return refresh_token;
 	}
 
 	public getRememberMe(): boolean {
-		const remember = localStorage.getItem(this.REMEMBER_ME);
+		const remember = localStorage.getItem(REMEMBER_ME);
 		if (remember !== null) {
 			return remember === 'true';
 		} else {
@@ -59,19 +59,19 @@ export default class LocalStorageService {
 	}
 
 	public removeJwt(): void {
-		localStorage.removeItem(this.ACCESS_TOKEN);
-		localStorage.removeItem(this.REFRESH_TOKEN);
-		localStorage.removeItem(this.EXPIRES_IN);
-		localStorage.removeItem(this.TIME_TOKEN_CREATE);
-		localStorage.removeItem(this.REMEMBER_ME);
+		localStorage.removeItem(ACCESS_TOKEN);
+		localStorage.removeItem(REFRESH_TOKEN);
+		localStorage.removeItem(EXPIRES_IN);
+		localStorage.removeItem(TIME_TOKEN_CREATE);
+		localStorage.removeItem(REMEMBER_ME);
 	}
 
 	public saveThemeMode(themeMode: string): void {
-		localStorage.setItem(this.THEME_MODE, themeMode);
+		localStorage.setItem(THEME_MODE, themeMode);
 	}
 
 	public getThemeMode(): PaletteMode {
-		const themeMode = localStorage.getItem(this.THEME_MODE) as PaletteMode;
+		const themeMode = localStorage.getItem(THEME_MODE) as PaletteMode;
 
 		return themeMode || 'light';
 	}
