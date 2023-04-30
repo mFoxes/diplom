d27afe1c3022fe651a9d2edeefbbbd85afd75fcc
Artem Evdokimov <>
2023-04-30 17:14:22 +0300

    refactor current employee service

--- src/diplomWeb/src/service/api/currentEmployeeService.ts
+++ src/diplomWeb/src/service/api/currentEmployeeService.ts
@@ -1,15 +1,26 @@
 import { AxiosResponse } from 'axios';
+import { AxiosApi } from './base/axiosApi';
+import { Either } from '@sweet-monads/either';
+import { ICurrentEmployee } from '../../models/interfaces/ICurrentEmployee';
+import overdueCountResponse from '../../models/interfaces/response/overdueCountResponse';
+import overdueListResponse from '../../models/interfaces/response/overdueListResponse';
 
-export default class CurrentEmployeeService {
-	async getCurrentEmployee(): Promise<AxiosResponse<ICurrentEmployee>> {
-		return $api.get('users/current');
+export default class CurrentEmployeeService extends AxiosApi {
+	public async getCurrentEmployee(): Promise<Either<unknown, ICurrentEmployee>> {
+		const req = this._get<ICurrentEmployee>({ url: 'users/current' });
+
+		return this._doApiRequest(req);
 	}
 
-	async getCurrentEmployeeOverdueCount(): Promise<AxiosResponse<overdueCountResponse>> {
-		return $api.get('bookings/overdue');
+	public async getCurrentEmployeeOverdueCount(): Promise<Either<unknown, overdueCountResponse>> {
+		const req = this._get<overdueCountResponse>({ url: 'bookings/overdue' });
+
+		return this._doApiRequest(req);
 	}
 
-	async getCurrentEmployeeOverdue(): Promise<AxiosResponse<overdueListResponse>> {
-		return $api.get('bookings/overdue/list');
+	public async getCurrentEmployeeOverdue(): Promise<Either<unknown, overdueListResponse>> {
+		const req = this._get<overdueListResponse>({ url: 'bookings/overdue/list' });
+
+		return this._doApiRequest(req);
 	}
 }
