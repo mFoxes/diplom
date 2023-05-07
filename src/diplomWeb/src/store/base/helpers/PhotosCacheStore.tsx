41e48b8bffc9978941b75aeb66416cf9da0f2d97
Artem Evdokimov <>
2023-05-07 22:15:30 +0300

    update photos cache store

--- src/diplomWeb/src/store/base/helpers/PhotosCacheStore.tsx
+++ src/diplomWeb/src/store/base/helpers/PhotosCacheStore.tsx
@@ -1,10 +1,15 @@
+import { inject } from 'inversify';
 import { makeAutoObservable } from 'mobx';
+import { Types } from '../../../inversify/inversify.types';
+import DownloadableImageService from '../../../service/api/downloadableImgService';
 
 interface IPhotoCache {
 	[key: string]: string;
 }
 
 export default class PhotosCacheStore {
+	@inject(Types.DownloadableImageService) private _downloadableImageService!: DownloadableImageService;
+
 	private _photos: IPhotoCache = {};
 
 	constructor() {
@@ -17,5 +22,16 @@ export default class PhotosCacheStore {
 
 	public setPhotoByKey(key: string, data: string): void {
 		this._photos[key] = data;
+	}
+
+	public async downloadImage(photoId: string): Promise<string | void> {
+		const res = await this._downloadableImageService.getPhotoById(photoId);
+
+		if (res.isRight()) {
+			const photo = URL.createObjectURL(res.value);
+			this.setPhotoByKey(photoId, photo);
+
+			return photo;
+		}
 	}
 }
